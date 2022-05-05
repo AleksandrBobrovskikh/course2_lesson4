5dbb08e0ff5344b2af2b836fa3725095fd92ee33
AleksandrBobrovskikh <bobrosan@gmail.com>
2022-05-05 18:38:36 +0300

    ДЗ от Бобровских А.В. 4 урок

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,9 +8,7 @@ import java.util.Optional;
 
 public class Controller {
 
-//    private Game game;
 
-//    private int step;
 
     @FXML
     private TextArea messageArea;
@@ -29,45 +27,4 @@ public class Controller {
         }
     }
 
-
-//    public Controller() {
-//        this.game = new Game();
-//        step = 0;
-//    }
-
-//    @FXML
-//    private void checkButtonClick(ActionEvent actionEvent) {
-//        String playerNumber = messageField.getText();
-//        if(playerNumber.isEmpty()){
-//            return;
-//        }
-//        Game.BullCowCount count = game.calcBullsAndCows(playerNumber);
-//        String text = String.format("%d. Введено число %s, быков %d, коров %d", ++step, playerNumber, count.getBulls(), count.getCows());
-//        messageArea.appendText(text + "\n");
-//        messageField.clear();
-//        messageField.requestFocus();
-//        if(count.getBulls() == 4) {
-//            if (isWantToPlayAgain()){
-//                step = 0;
-//                messageArea.clear();
-//                this.game = new Game();
-//            } else {
-//                System.exit(0);
-//            }
-//        }
-//    }
-
-//    private boolean isWantToPlayAgain() {
-//        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Поздравляю с победой\n" + "Загаданное число " + game.getGuessNumber() + "Еще раз",
-//                new ButtonType("Yes", ButtonBar.ButtonData.YES),
-//                new ButtonType("No", ButtonBar.ButtonData.NO));
-//        alert.setTitle("Вопрос");
-//        ButtonType answer = alert.showAndWait().get();
-//        return answer.getButtonData() == ButtonBar.ButtonData.YES;
-//
-//    }
-
-//    public void menuExit(ActionEvent actionEvent) {
-//        System.exit(0);
-//    }
 }
