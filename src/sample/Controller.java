848e1d0dc906797a5b5dd2aba4f822d0cd819348
AleksandrBobrovskikh <bobrosan@gmail.com>
2022-05-05 18:36:10 +0300

    ДЗ от Бобровских А.В. 4 урок

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,73 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.util.Optional;
+
 public class Controller {
+
+//    private Game game;
+
+//    private int step;
+
+    @FXML
+    private TextArea messageArea;
+    @FXML
+    private TextField messageField;
+
+
+    public void checkButtonClick(ActionEvent actionEvent) {
+        if (!messageField.getText().isEmpty()) {
+            if (messageField.getText().equals("/exit"))
+                System.exit(0);
+            else {
+                messageArea.appendText(messageField.getText() + "\n");
+                messageField.clear();
+            }
+        }
+    }
+
+
+//    public Controller() {
+//        this.game = new Game();
+//        step = 0;
+//    }
+
+//    @FXML
+//    private void checkButtonClick(ActionEvent actionEvent) {
+//        String playerNumber = messageField.getText();
+//        if(playerNumber.isEmpty()){
+//            return;
+//        }
+//        Game.BullCowCount count = game.calcBullsAndCows(playerNumber);
+//        String text = String.format("%d. Введено число %s, быков %d, коров %d", ++step, playerNumber, count.getBulls(), count.getCows());
+//        messageArea.appendText(text + "\n");
+//        messageField.clear();
+//        messageField.requestFocus();
+//        if(count.getBulls() == 4) {
+//            if (isWantToPlayAgain()){
+//                step = 0;
+//                messageArea.clear();
+//                this.game = new Game();
+//            } else {
+//                System.exit(0);
+//            }
+//        }
+//    }
+
+//    private boolean isWantToPlayAgain() {
+//        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Поздравляю с победой\n" + "Загаданное число " + game.getGuessNumber() + "Еще раз",
+//                new ButtonType("Yes", ButtonBar.ButtonData.YES),
+//                new ButtonType("No", ButtonBar.ButtonData.NO));
+//        alert.setTitle("Вопрос");
+//        ButtonType answer = alert.showAndWait().get();
+//        return answer.getButtonData() == ButtonBar.ButtonData.YES;
+//
+//    }
+
+//    public void menuExit(ActionEvent actionEvent) {
+//        System.exit(0);
+//    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,13 +9,13 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Lesson №4");
+        primaryStage.setScene(new Scene(root, 300, 400));
         primaryStage.show();
-    }
 
+    }
 
     public static void main(String[] args) {
         launch(args);
